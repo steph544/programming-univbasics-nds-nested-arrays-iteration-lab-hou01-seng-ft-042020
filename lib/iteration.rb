51a38730981f9097c5b9a848db05369b54103fde
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-21 03:30:34 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,54 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+ row_index= 0
+ inner_results = []
+  while row_index < src.count do 
+    element_index=0 
+    while element_index <
+    src[row_index].count do
+       inner_results << "I love #{src[row_index][element_index]} and #{src[row_index][element_index += 1]} on my pizza"
+      element_index +=1 
+    end
+    row_index += 1
+  end
+  inner_results
 end
+
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  row_index=0 
+  inner_results = []
+  while row_index < src.count do
+    element_index=0
+    x = src[row_index][element_index]
+    y = src [row_index][element_index += 1]
+    while element_index<src[row_index].count do
+      if x > y
+      inner_results << x
+      else x < y
+      inner_results <<  y
+      end
+    element_index +=1 
+    end
+    row_index += 1 
+ end
+  inner_results
 end
 
+
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total= 0 
+  row_index=0 
+  while row_index<src.count do
+      element_index=0 
+      while element_index<src[row_index].count do
+        x = src[row_index][element_index]
+        y = src [row_index][element_index += 1]
+        if (x%2) and (y%2) == 0
+        total+= (x+y)
+        end
+      element_index += 1 
+      end
+    row_index +=1 
+  end
+ total
 end
