bab2887a65476b83c1f2be9b6a4c73fb18126ffc
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-21 02:47:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,10 +1,8 @@
 def join_ingredients(src)
  row_index= 0
-  outer_results=[]
   while row_index < src.count do 
     element_index=0 
     inner_results = []
-    row_index=0
     while element_index <
     src[row_index].count do
        inner_results << src[row_index][element_index]
