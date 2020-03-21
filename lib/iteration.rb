bbf41b6b668df6b33b24ddccbea4b700e6f62eca
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-21 03:29:52 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -41,8 +41,9 @@ def total_even_pairs(src)
   while row_index<src.count do
       element_index=0 
       while element_index<src[row_index].count do
-        x = src[row_index][element_index] + src [row_index][element_index += 1]
-        if (x%2)== 0 
+        x = src[row_index][element_index]
+        y = src [row_index][element_index += 1]
+        if (x%2) and (y%2) == 0
         total+=x
         end
       element_index += 1 
